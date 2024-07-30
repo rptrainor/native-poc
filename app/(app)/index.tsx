0e6ce46bad5c47e98772177eec36cebc9b0e40e0
Ryan Trainor <rptrainor@gmail.com>
2024-07-30 12:42:17 -0300

    link externally

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,20 +1,38 @@
-import { WebView } from 'react-native-webview';
-import Constants from 'expo-constants';
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { WebView, type WebViewNavigation } from "react-native-webview";
+import Constants from "expo-constants";
+import { StyleSheet, Linking } from "react-native";
 
 export default function App() {
-  return (
-    <WebView
-      style={styles.container}
-      originWhitelist={['*']}
-      source={{ html: '<div><h1><center>Hello world</center></h1><ul><li><a href="https://www.google.com">https://www.google.com</a></li><li><a href="https://app.ofcolor.com">https://app.ofcolor.com</a></li></ul></div>' }}
-    />
-  );
+	const handleNavigationStateChange = (navState: WebViewNavigation) => {
+		const { url } = navState;
+
+		if (url.startsWith("https://www.google.com")) {
+			// Prevent WebView from loading the URL
+			webViewRef.current?.stopLoading();
+			// Open the URL in the device's default browser
+			Linking.openURL(url);
+		}
+	};
+
+	const webViewRef = React.useRef<WebView>(null);
+
+	return (
+		<WebView
+			ref={webViewRef}
+			style={styles.container}
+			originWhitelist={["*"]}
+			source={{
+				html: '<div><h1><center>Hello world</center></h1><ul><li><a href="https://www.google.com">https://www.google.com</a></li><li><a href="https://app.ofcolor.com">https://app.ofcolor.com</a></li></ul></div>',
+			}}
+			onNavigationStateChange={handleNavigationStateChange}
+		/>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-  },
+	container: {
+		flex: 1,
+		marginTop: Constants.statusBarHeight,
+	},
 });
